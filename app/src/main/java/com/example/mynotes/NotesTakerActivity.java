367691e0449f89ced675c679226830bcdd6bb663
SfShev <pshev1406@gmail.com>
2023-05-16 17:47:01 +0300

    ability to adding and saving notes

--- app/src/main/java/com/example/mynotes/NotesTakerActivity.java
+++ app/src/main/java/com/example/mynotes/NotesTakerActivity.java
@@ -2,14 +2,24 @@ package com.example.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.mynotes.Models.Notes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NotesTakerActivity extends AppCompatActivity {
 
     EditText editText_title,editText_notes;
     ImageView imageView_save;
+    Notes notes;
 
 
     @Override
@@ -21,6 +31,31 @@ public class NotesTakerActivity extends AppCompatActivity {
         editText_notes = findViewById(R.id.editText_notes);
 
         imageView_save = findViewById(R.id.imageView_save);
+        imageView_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = editText_title.getText().toString();
+                String description = editText_notes.getText().toString();
+
+                if (description.isEmpty()){
+                    Toast.makeText(NotesTakerActivity.this,"Please enter description",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                SimpleDateFormat formater = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
+                Date date = new Date();
+
+                notes = new Notes();
+                notes.setTitle(title);
+                notes.setNotes(description);
+                notes.setDate(formater.format(date));
+
+
+                Intent intent = new Intent();
+                intent.putExtra("note", notes);
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+            }
+        });
 
     }
 }
